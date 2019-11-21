72563d1026147e49695d5cbde41d4696dd8781ab
RicardoTaipe <rdavid28@hotmail.es>
2019-11-20 19:46:40 -0500

    all services

---
+++ microservice1-1.0/index.js
@@ -0,0 +1,48 @@
+var restify = require("restify");
+var errors = require("restify-errors");
+var server = restify.createServer();
+
+server.use(restify.plugins.bodyParser());
+server.use(restify.plugins.authorizationParser());
+/*
+server.use(function(req, res, next) {
+  users = {
+    ibm_test_robot_1: { password: "ibm_test_1.123#" },
+    ibm_test_robot_2: { password: "ibm_test_2.123#" },
+    ibm_test_robot_3: { password: "ibm_test_3.123#" }
+  };
+  if (
+    users[req.username] &&
+    req.authorization.basic.password == users[req.username].password
+  ) {
+    console.log("passed");
+    return next();
+  } else {
+    console.log("not passed");
+    return next(new errors.NotAuthorizedError());
+  }
+});
+*/
+
+server.use(function(err, req, res, next) {
+  console.error(err);
+  const error_result = { status: "error", message: err.message };
+  res.send(404, error_result);
+});
+
+server.post("/ibmchallengemic1/element_sorter", sendV100);
+
+function sendV100(req, res, next) {
+  var { elements } = req.body;
+  const success_response = {
+    status: "success",
+    message: "ok",
+    data: { sorted: elements.sort() }
+  };
+  res.send(200, success_response);
+  return next();
+}
+
+server.listen(8080, function() {
+  console.log(server.name + " listening at " + server.url);
+});
---
+++ microservice2-1.1/index.js
@@ -0,0 +1,61 @@
+var restify = require("restify");
+var errors = require("restify-errors");
+var server = restify.createServer();
+const { mode, min, max, mean, std } = require("mathjs");
+
+server.use(restify.plugins.bodyParser());
+server.use(restify.plugins.authorizationParser());
+/*
+server.use(function(req, res, next) {
+  users = {
+    ibm_test_robot_1: { password: "ibm_test_1.123#" },
+    ibm_test_robot_2: { password: "ibm_test_2.123#" },
+    ibm_test_robot_3: { password: "ibm_test_3.123#" }
+  };
+  if (
+    users[req.username] &&
+    req.authorization.basic.password == users[req.username].password
+  ) {
+    console.log("passed");
+    return next();
+  } else {
+    console.log("not passed");
+    return next(new errors.NotAuthorizedError());
+  }
+});
+*/
+server.use(function(err, req, res, next) {
+  console.error(err);
+  const error_result = { status: "error", message: err.message };
+  res.send(404, error_result);
+});
+
+server.post("/ibmchallengemic2/statistics", statistics);
+
+function statistics(req, res, next) {
+  var { elements } = req.body;
+  var discardedElements = elements.filter(
+    element => typeof element !== "number"
+  );
+  var numbers = elements.filter(element => typeof element === "number");
+  const success_response = {
+    status: "success",
+    message: "ok",
+    data: {
+      mode: mode(numbers),
+      average: mean(numbers),
+      min: min(numbers),
+      max: max(numbers),
+      mean: mean(numbers),
+      count: numbers.length,
+      stdev: std(numbers),
+      discardedElements
+    }
+  };
+  res.send(200, success_response);
+  return next();
+}
+
+server.listen(8081, function() {
+  console.log(server.name + " listening at " + server.url);
+});
